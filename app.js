04427ce7c0e74a3366e24a3a42d21bfc11c07ae7
ImmortalLark <frorice@163.com>
2018-09-17 16:47:09 +0800

    1.增加错误处理 2.修改连接池维护逻辑

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
  * @Author: Feng fan
  * @Date: 2018-09-05 10:53:38
  * @Last Modified by: Feng fan
- * @Last Modified time: 2018-09-13 16:01:07
+ * @Last Modified time: 2018-09-17 14:52:46
  */
-const Portal = require('./bin/Portal');
+const Portal = require('./lib/Portal');
 
 module.exports = ({
     remoteHost, // 远端服务地址
@@ -13,7 +13,7 @@ module.exports = ({
     remoteConnectPort, // 指定的远端连接端口，为空则随机分配
     localHost = 'localhost', // 本地项目host
     localPort = 80, // 本地项目端口
-    maxConnectionCount = 30 // 最大连接数
+    maxConnectionCount = 10 // 最大连接数
 }) => {
     const portal = new Portal({
         remoteHost, 
