20fedd2c648778a888b03c20794d1edbfa65aab1
ImmortalLark <frorice@163.com>
2018-11-26 09:52:51 +0800

    重构代码为websocket实现

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
  * @Author: Feng fan
  * @Date: 2018-09-05 10:53:38
  * @Last Modified by: Feng fan
- * @Last Modified time: 2018-09-17 14:52:46
+ * @Last Modified time: 2018-11-23 15:13:10
  */
 const Portal = require('./lib/Portal');
 
 module.exports = ({
     remoteHost, // 远端服务地址
-    remotePort = 80, // 远端服务端口
+    remotePort = 80, // 远端http服务端口
     subdomain, // 指定的子域名,为空则随机分配
-    remoteConnectPort, // 指定的远端连接端口，为空则随机分配
+    remoteConnectPort = 2000, // 远端websocket连接端口
     localHost = 'localhost', // 本地项目host
     localPort = 80, // 本地项目端口
     maxConnectionCount = 10 // 最大连接数
