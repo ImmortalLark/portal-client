4b54821d85888919f2cd5d39b449ccb8413ed47c
ImmortalLark <frorice@163.com>
2018-11-26 17:39:48 +0800

    增加数据解压缩功能

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: Feng fan
  * @Date: 2018-09-05 10:53:38
  * @Last Modified by: Feng fan
- * @Last Modified time: 2018-11-23 15:13:10
+ * @Last Modified time: 2018-11-26 16:42:10
  */
 const Portal = require('./lib/Portal');
 
@@ -10,7 +10,7 @@ module.exports = ({
     remoteHost, // 远端服务地址
     remotePort = 80, // 远端http服务端口
     subdomain, // 指定的子域名,为空则随机分配
-    remoteConnectPort = 2000, // 远端websocket连接端口
+    remoteConnectPort = 80, // 远端websocket连接端口
     localHost = 'localhost', // 本地项目host
     localPort = 80, // 本地项目端口
     maxConnectionCount = 10 // 最大连接数
